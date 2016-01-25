12fa345a9af85b78e21798aaf80812ff307af879
Chobits <chobits@live.cn>
2016-01-25 09:45:08 +0800

    add FileLogger

---
+++ Rest.Api/Util/FileLogger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace Rest.Api.Util
+{
+    public class FileLogger
+    {
+        //const string LOG_FILE_NAME = "service.log";
+        const string DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+        public FileLogger(string filePath)
+        {
+            try
+            {
+                Trace.Listeners.Add(new TextWriterTraceListener(filePath));
+            }
+            catch //(Exception e)  
+            {
+                Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            }
+            Trace.AutoFlush = true;
+        }
+
+        #region interface  
+
+        public void Error(string message)
+        {
+            Trace.WriteLine(message, DateTime.Now.ToString(DATETIME_FORMAT) + " ERROR");
+        }
+
+        public void Warn(string message)
+        {
+            Trace.WriteLine(message, DateTime.Now.ToString(DATETIME_FORMAT) + " WARN");
+        }
+
+        public void Info(string message)
+        {
+            Trace.WriteLine(message, DateTime.Now.ToString(DATETIME_FORMAT) + " INFO");
+        }
+
+        #endregion
+
+    }
+}
